4a072fea82d7ec1f50283d91bb1071aba896bb81
Mels <m.c.vanes@tilburguniversity.edu>
2021-01-25 22:45:55 +0100

    Doors and movement fixed

---
+++ HCI GAME/Assets/DoorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+   private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "SF_Door")
+        {
+            Animator anim = other.GetComponentInChildren<Animator>();
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                anim.SetTrigger("OpenClose");
+            }
+            
+        }
+    }
+}
