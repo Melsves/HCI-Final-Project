580ab70d405b35aa8276036934fe6b0316f65127
aniela <aniela200>
2021-01-27 16:07:11 +0100

    Added LevelBar (rn it works on pressing the spacebar)

---
+++ HCI GAME/Assets/barr/Assets/PlayerLevel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLevel : MonoBehaviour
+{
+
+	public int minLevel = 0;
+	public int currentLevel;
+
+	public levelBar levelBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		currentLevel = minLevel;
+		levelBar.SetMinLevel(minLevel);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Addlevel(1);
+		}
+    }
+
+
+	void Addlevel(int level)
+	{
+		currentLevel += level;
+
+		levelBar.SetLevel(currentLevel);
+	}
+}
