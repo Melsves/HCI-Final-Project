f0088b577b50f0325f1afa549880411067765e40
Mels <m.c.vanes@tilburguniversity.edu>
2021-01-29 10:51:39 +0100

    Controller movement

--- HCI GAME/Assets/move.cs
+++ HCI GAME/Assets/move.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    public CharacterController controller;
 
-    public float speed = 12f;
 
-    // Update is called once per frame
-    void Update()
+    public CharacterController _controller;
+    public float _speed = 10;
+    public float _rotationSpeed = 180;
+
+    private Vector3 rotation;
+
+    public void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        this.rotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * _rotationSpeed * Time.deltaTime, 0);
 
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * speed * Time.deltaTime);
+        Vector3 move = new Vector3(0, 0, Input.GetAxisRaw("Vertical") * Time.deltaTime);
+        move = this.transform.TransformDirection(move);
+        _controller.Move(move * _speed);
+        this.transform.Rotate(this.rotation);
     }
 }
